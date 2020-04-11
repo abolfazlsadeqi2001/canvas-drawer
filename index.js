90f926e1722ab3493ecf486f8669c5962696f364
abol <abolfazlsadeqi2001@chmail.ir>
2020-04-11 17:33:02 +0430

    add eraser

--- index.js
+++ index.js
@@ -3,15 +3,16 @@ var currentPosition = {x:0,y:0};
 
 var canvas;
 var ctx;
-var canvasDimensions = {width:840,height:420};
+var canvasDimensions = {width:930,height:420};
+
+var markerSize = 28;
 
 var isWriting = false;
 
 var lineWidth;
 var color;
-
 var pencilWidth = 2;
-var eraserInput;
+var eraserWidth;
 
 function init(){
 	// configure the canvas
@@ -20,5 +21,25 @@ function init(){
 	canvas.width = canvasDimensions.width;
 	canvas.height = canvasDimensions.height;
 	// configure markers
-	var containerOfMarkers = document.querySelector(".marker-container");
+	var markers = document.querySelectorAll(".marker-container *");
+	markers.forEach(marker =>{
+		marker.style.backgroundColor = marker.getAttribute("color");
+		marker.style.width = markerSize;
+		marker.style.height = markerSize;
+		marker.addEventListener("click",e=>{
+			color = marker.getAttribute("color");
+			lineWidth = pencilWidth;
+		});
+	});
+	// configure eraser
+	var eraserInput = document.querySelector("[type='number']");
+	eraserInput.addEventListener("change",e=>{
+		eraserWidth = eraserInput.value;
+	});
+	
+	var eraserButton = document.querySelector("#eraser");
+	eraserButton.addEventListener("click",e=>{
+		color = "white";
+		lineWidth = eraserWidth;
+	});
 }
