7b17b69cd15a8ad26b72e340b9ceb04b43b344a2
abol <abolfazlsadeqi2001@chmail.ir>
2020-04-11 17:02:20 +0430

    write the basics of js file

---
+++ index.js
@@ -0,0 +1,17 @@
+var previousPosition = {x:0,y:0};
+var currentPosition = {x:0,y:0};
+
+var canvas;
+var ctx;
+var canvasDimensions = {width:840,height:420};
+
+var isWriting = false;
+
+var lineWidth;
+var color;
+var pencilWidth = 2;
+var eraserInput;
+
+function init(){
+	
+}
