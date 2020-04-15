0a56722435e5a6b8f8011db8e12fd1bfdba537a5
abol <abolfazlsadeqi2001@chmail.ir>
2020-04-15 10:07:34 +0430

    change the max size to 100kB

--- white-board-deployer/src/main/java/sockets/stream/WebSocketParent.java
+++ white-board-deployer/src/main/java/sockets/stream/WebSocketParent.java
@@ -1,6 +1,6 @@
 package sockets.stream;
 
 public class WebSocketParent {
-	protected static final int MAX_TEXT_MESSAGE_SIZE = 40 * 1024;// max gained text message is 35 kB
+	protected static final int MAX_TEXT_MESSAGE_SIZE = 100 * 1024;// max gained text message is 35 kB
 	protected static final int TIME_OUT_PER_MILI_SECONDS = 10 * 1000;// sync time is 5	
 }
